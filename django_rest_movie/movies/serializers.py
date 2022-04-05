671809c35323cc2cf6364bf3779086682cdfaa3e
Sumr0ck <sumrock1@mail.ru>
2022-04-05 16:28:22 +0300

    add rating for movie

--- django_rest_movie/movies/serializers.py
+++ django_rest_movie/movies/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Movie, Review
+from .models import Movie, Rating, Review
 
 
 class FilterReviewSerializer(serializers.ListSerializer):
@@ -61,3 +61,19 @@ class MovieDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         exclude = ('draft', )
+
+
+class CreateRatingSerializer(serializers.ModelSerializer):
+    """Добавление рейтинга фильма"""
+
+    def create(self, validated_data):
+        rating = Rating.objects.update_or_create(
+            ip=validated_data.get('ip', None),
+            movie=validated_data.get('movie', None),
+            defaults={'star': validated_data.get('star')}
+        )
+        return rating
+
+    class Meta:
+        model = Rating
+        fields = ('star', 'movie')
